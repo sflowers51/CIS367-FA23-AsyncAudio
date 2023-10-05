12d58773abd685c97a3d5a4c350b4aff3a21839c
sflowers51 <samuel.flowers@students.fhu.edu>
2023-10-04 23:53:34 -0500

    yes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio:{
+        'square': '100%',
+      }
+    },
   },
   plugins: [],
 }
